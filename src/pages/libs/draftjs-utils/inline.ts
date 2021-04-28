47260ae0a28f3330433ffadb5e7987a394beeffd
Yuurilee <yurilee9696@gmail.com>
2021-04-29 00:10:14 +0900

    Refactor: 오타 수정

--- src/pages/libs/draftjs-utils/inline.ts
+++ src/pages/libs/draftjs-utils/inline.ts
@@ -225,7 +225,7 @@ export const getSelectionCustomInlineStyle = (
   return {};
 };
 
-interface ChangeOriginalStyleToNeweStyle {
+interface ChangeOriginalStyleToNewStyle {
   editorState: EditorState;
   originalStyle: StyleKeyType;
   newStyle?: StyleKeyType;
@@ -241,7 +241,7 @@ export const changeOriginalStyleToNeweStyle = ({
   newStyle,
   startOffset,
   endOffset,
-}: ChangeOriginalStyleToNeweStyle): EditorState => {
+}: ChangeOriginalStyleToNewStyle): EditorState => {
   const selectionState = editorState.getSelection();
   const originalSelection = selectionState.merge({
     anchorKey: selectionState.getAnchorKey(),
